5b109a126c8b16f8909b934c139bdd1ff071607a
Laurent Guerin <laguerin@LFR002634.corp.capgemini.com>
2016-07-08 17:57:55 +0200

    code formatting

--- src/org/telosys/tools/eclipse/plugin/commons/dialogbox/GenerationTaskMsgBox.java
+++ src/org/telosys/tools/eclipse/plugin/commons/dialogbox/GenerationTaskMsgBox.java
@@ -9,14 +9,34 @@ import org.eclipse.jface.dialogs.MessageDialog;
 public class GenerationTaskMsgBox
 {
 	//----------------------------------------------------------------------------------
-	public static boolean error(String title, String message, Throwable e) 
+	public static boolean error(String title, String message, Throwable exception) 
 	{
-		MessageDialog dialog = new MessageDialog(null, 
-				title, null, 
-				message, MessageDialog.ERROR, 
-				new String[]{
-				"Continue", "Cancel" }, 0); 
+		MessageDialog dialog = new MessageDialog(
+				null, 
+				title, 
+				null, 
+				buildMessage(message, exception),
+				MessageDialog.ERROR, 
+				new String[]{ "Continue", "Cancel" }, 
+				0); 
 		int r = dialog.open();
 		return r == 0; // TRUE if "continue", else FALSE
+	}
+	
+	private static String buildMessage(String message, Throwable exception) {
+		StringBuffer sb = new StringBuffer();
+		sb.append(message);
+		sb.append("\n");
+		if ( exception != null ) {
+			if ( exception.getCause() != null ) {
+				Throwable cause = exception.getCause() ;
+				//sb.append("Exception : ") ;
+				sb.append(cause.getClass().getSimpleName() ) ;
+				sb.append(" : \n");
+				sb.append(cause.getMessage() ) ;
+				sb.append("\n");
+			}
+		}
+		return sb.toString();
 	}
 }
