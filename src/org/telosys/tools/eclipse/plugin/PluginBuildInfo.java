d351c56fa26683e128d358c2601bc2bae4f852f9
Laurent Guerin <laguerin@LFR002634.corp.capgemini.com>
2016-08-11 10:47:59 +0200

    Version 'RC3'

--- src/org/telosys/tools/eclipse/plugin/PluginBuildInfo.java
+++ src/org/telosys/tools/eclipse/plugin/PluginBuildInfo.java
@@ -23,7 +23,7 @@ package org.telosys.tools.eclipse.plugin;
  */
 public class PluginBuildInfo {
 
-	public final static String BUILD_ID   = "RC2" ;
+	public final static String BUILD_ID   = "RC3" ;
 
-	public final static String BUILD_DATE = "2016-06-07" ;
+	public final static String BUILD_DATE = "2016-08-11" ;
 }
